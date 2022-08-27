c6c5bb4ee701b561a002c5980a5bbe79da940d26
yasnish <yasuki.nishinaka@gmail.com>
2022-08-27 17:03:15 +0900

    Add redux-logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import logger from 'redux-logger';
+
+const reducer = {
+  counter: counterReducer,
+};
+const preloadedState = {};
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState,
 });
 
 export type AppDispatch = typeof store.dispatch;
